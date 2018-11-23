ca3e8a424989e8608319600190c12f1a46337e35
takumi.yamamoto <rocca1141@gmail.com>
2018-11-23 12:08:07 +0900

    ファイルのコピーなど

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {HeroesRoutingModule} from './heroes-routing.module';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import {HeroListComponent} from './hero-list/hero-list.component';
+
+
+@NgModule({
+    declarations: [
+        HeroDetailComponent,
+        HeroListComponent
+    ],
+    imports: [
+        CommonModule,
+        HeroesRoutingModule
+    ]
+})
+export class HeroesModule {
+}
